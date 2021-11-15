e7af46a96900be7e9715b1e5ddc71d497f49fce1
mlowell28 <49284072+mlowell28@users.noreply.github.com>
2021-11-14 22:47:31 -0800

    Create Fisheye_Transform.py

---
+++ Fisheye_Transform.py
@@ -0,0 +1,55 @@
+import scipy 
+import numpy as np
+import torch
+import math
+
+class fisheye_generator():
+    
+    def __init__(self, sphere_radius, focal_distance = None):
+        
+        self.sphere_radius = sphere_radius
+        if self.sphere_radius < math.sqrt(2):
+           self.sphere_radius = math.sqrt(2)
+        
+        self.focal_distance = focal_distance
+        if self.focal_distance == None:
+            self.focal_distance = self.sphere_radius
+
+    def fisheye_transform(self, image):
+        
+        image_dims = len(image.shape)
+        
+        # add batch and channel dimension if missing
+        if len(image.shape) <4:
+            image = torch.unsqueeze(image, 0)
+        
+        if len(image.shape) <4:
+            image = torch.unsqueeze(image, 0)
+        
+        (b,c,h,w) = image.shape
+        
+        h_points = torch.linspace(-1,1,h)
+        w_points = torch.linspace(-1,1,w)
+        
+        h_grid, w_grid = torch.meshgrid(h_points, w_points)
+        
+        C_grid = 1/torch.sqrt(self.sphere_radius**2 - (h_grid**2 + w_grid**2))*self.focal_distance
+        
+        h_sample_grid = C_grid*h_grid
+        w_sample_grid = C_grid*w_grid
+        
+        sample_tensor = torch.stack((w_sample_grid, h_sample_grid),2)
+        sample_tensor = torch.unsqueeze(sample_tensor, 0)
+        sample_tensor = torch.tile(sample_tensor, (b,1,1,1))
+        
+        fisheye_images = torch.nn.functional.grid_sample(image, sample_tensor)
+        
+        if image_dims <= 3:
+            fisheye_images = torch.squeeze(fisheye_images, dim=0)
+        if image_dims == 2:
+            fisheye_images = torch.squeeze(fisheye_images, dim=0)
+        
+        return fisheye_images
+        
+        
+        
